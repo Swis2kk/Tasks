9b5d047d52f088de6409cef9aff8f95f8c90b0c3
d2k <email@mail.com>
2018-04-03 21:05:06 +0300

    03.04.18 methods accept generic types

--- src/main/java/org/learning/findKth/Sample.java
+++ src/main/java/org/learning/findKth/Sample.java
@@ -1,14 +1,17 @@
 package org.learning.findKth;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class Sample {
 
     public static <T> T kth(List<T> elements, int k) {
-        if (elements.size() >= k) {
+        if (elements.size() >= 1) {
             elements.sort(Comparator.comparing(s -> s.hashCode()));
+            int bigest = elements.get(elements.size()-1).hashCode();
+            if (k > bigest) {
+                throw new IllegalArgumentException("Incorrect entry in k");
+            }
             return elements.get(k);
         } else {
             throw new IllegalArgumentException("Empty Entry");
