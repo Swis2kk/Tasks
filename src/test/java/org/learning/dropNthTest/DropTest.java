dc6e8d535eea831d1858c4bc5d0fc5ac7275fca5
dnedealcov <dnedealcov@endava.com>
2018-04-05 11:40:09 +0300

    05.04.18 - new task dropNth

--- src/test/java/org/learning/dropNthTest/DropTest.java
+++ src/test/java/org/learning/dropNthTest/DropTest.java
@@ -25,5 +25,12 @@ public class DropTest {
         assertThat(result).contains("a", "b");
     }
 
-    
+    @Test
+    public void shouldReturnSameListWhenNIsZero() throws Exception {
+        List<String> result = Drop.dropEveryNth(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"), 0);
+        assertThat(result).hasSize(11);
+        assertThat(result).contains("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k");
+    }
+
+
 }
